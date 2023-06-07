2f32438accba82b0c3a4148dbd8c2873e4d01d16
Faizan <122879780+khanfaiz0@users.noreply.github.com>
2023-06-07 11:51:10 +0530

    Update database.js

--- data/database.js
+++ data/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-export const datadb = () => mongoose.connect(process.env.mongo_uri,{
+export const datadb = () => mongoose.connect(process.env.MONGO_URI,{
     dbName: "backendapi",
 }).then(() => console.log("database connected")).catch((e) => console.log(e))
